3193ba98a62fc46cfd7ab5c1b6e2b7116fb11d05
SuhailSevesanker <suhailsevesanker9@gmail.com>
2023-02-16 17:30:52 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unlstd.h>
 /**
  * main - Entry Point
  * Return: Always 1
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Kopar, 2015-10-19\n, stdout);
+	fput("and that piece of art is useful\" - Dora Kopar, 2015-10-19\n, stdout);
 	return (1);
 }
